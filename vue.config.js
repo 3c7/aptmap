91c421284c776177d5c67da27c32603f9d461a80
Nils Kuhnert <Nils.Kuhnert@cert-bund.de>
2018-08-06 16:28:31 +0200

    WIP: include jquery & bootstrap in file

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,15 @@
+const HtmlWebpackIncludeAssetsPlugin = require('html-webpack-include-assets-plugin');
 const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 module.exports = {
     chainWebpack: config => {
         if (process.env.NODE_ENV === 'production') {
-            config.plugin('html-inline').use(HtmlWebpackInlineSourcePlugin);
+            config.plugin('html-assets').use(HtmlWebpackIncludeAssetsPlugin)
+            config.plugin('html-assets').tap(args => {
+                args[0].assets = ['css/bootstrap.min.css', 'css/bootstrap.min.css.map', 'js/jquery-3.3.1.slim.min.js', 'js/popper.min.js', 'bootstrap.min.js']
+                args[0].append = false
+                return args
+            })
+            config.plugin('html-inline').use(HtmlWebpackInlineSourcePlugin)
             config.plugin('html').tap(args => {
                 args[0].inlineSource = '.(js|css)$'
                 return args
